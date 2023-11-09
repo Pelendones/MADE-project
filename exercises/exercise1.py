6fa9b2187c16ff3284ae458584c344072c88c727
Pelendones <jean-philipp.schaller@fau.de>
2023-11-09 11:41:21 +0100

    added exercise1

---
+++ exercises/exercise1.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Daten aus csv einlesen
+df = pd.read_csv('rhein-kreis-neuss-flughafen-weltweit.csv', sep=';') 
+
+#Schreiben in eine sqlite Datei mit dem Namen airports.sqlite und dem Tabellennamen airports und manuelles setzen der Datentypen BIGINT, TEXT or FLOAT
+df.to_sql('airports','sqlite:///airports.sqlite',if_exists='replace', index=False, dtype={
+        'column_1':'INT',
+        'column_2': 'TEXT',
+        'column_3': 'TEXT',
+        'column_4': 'TEXT',
+        'column_5': 'TEXT ',
+        'column_6': 'TEXT',
+        'column_7': 'FLOAT',
+        'column_8': 'FLOAT',
+        'column_9': 'INT',
+        'column_10': 'TEXT',
+        'column_11': 'TEXT',
+        'column_12': 'TEXT',
+        'geo_punkt': 'TEXT'
+})
