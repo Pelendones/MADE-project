235f9511aa11c644b93452d1afd6a9dae0bf5dc7
Pelendones <jean-philipp.schaller@fau.de>
2023-11-09 12:26:35 +0100

    new code exercise 1

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,21 +1,24 @@
-import pandas as pd
+import pandas as pd 
 
 # Daten aus csv einlesen
-df = pd.read_csv('rhein-kreis-neuss-flughafen-weltweit.csv', sep=';') 
+df = pd.read_csv('https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv', sep=';') 
+
+dtype_dict = {
+    'column_1': 'INTEGER',
+    'column_2': 'TEXT',
+    'column_3': 'TEXT',
+    'column_4': 'TEXT',
+    'column_5': 'TEXT',
+    'column_6': 'TEXT',
+    'column_7': 'REAL',
+    'column_8': 'REAL',
+    'column_9': 'INTEGER',
+    'column_10': 'TEXT',
+    'column_11': 'TEXT',
+    'column_12': 'TEXT',
+    'geo_punkt': 'TEXT'
+} 
 
 #Schreiben in eine sqlite Datei mit dem Namen airports.sqlite und dem Tabellennamen airports und manuelles setzen der Datentypen BIGINT, TEXT or FLOAT
-df.to_sql('airports','airports.sqlite',if_exists='replace', index=False, dtype={
-        'column_1':'INT',
-        'column_2': 'TEXT',
-        'column_3': 'TEXT',
-        'column_4': 'TEXT',
-        'column_5': 'TEXT ',
-        'column_6': 'TEXT',
-        'column_7': 'FLOAT',
-        'column_8': 'FLOAT',
-        'column_9': 'INT',
-        'column_10': 'TEXT',
-        'column_11': 'TEXT',
-        'column_12': 'TEXT',
-        'geo_punkt': 'TEXT'
-})
+df.to_sql('airports','sqlite:///airports.sqlite',if_exists='replace', index=False, #dtype=dtype_dict
+          )
