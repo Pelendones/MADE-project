cb709e404706ae23c392ec2fdd0c2789ff64b77d
Pelendones <jean-philipp.schaller@fau.de>
2023-11-09 12:28:29 +0100

    Removed unnecessary code from exercise 1

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,24 +1,7 @@
 import pandas as pd 
 
-# Daten aus csv einlesen
+# Daten aus csv Quelle einlesen
 df = pd.read_csv('https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv', sep=';') 
 
-dtype_dict = {
-    'column_1': 'INTEGER',
-    'column_2': 'TEXT',
-    'column_3': 'TEXT',
-    'column_4': 'TEXT',
-    'column_5': 'TEXT',
-    'column_6': 'TEXT',
-    'column_7': 'REAL',
-    'column_8': 'REAL',
-    'column_9': 'INTEGER',
-    'column_10': 'TEXT',
-    'column_11': 'TEXT',
-    'column_12': 'TEXT',
-    'geo_punkt': 'TEXT'
-} 
-
-#Schreiben in eine sqlite Datei mit dem Namen airports.sqlite und dem Tabellennamen airports und manuelles setzen der Datentypen BIGINT, TEXT or FLOAT
-df.to_sql('airports','sqlite:///airports.sqlite',if_exists='replace', index=False, #dtype=dtype_dict
-          )
+#Schreiben in eine sqlite Datei mit dem Namen airports.sqlite und dem Tabellennamen airports
+df.to_sql('airports','sqlite:///airports.sqlite',if_exists='replace', index=False )
