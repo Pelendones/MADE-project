6e9277e5242f8762325d4bb9d682ad9dc5a8713b
Pelendones <jean-philipp.schaller@fau.de>
2023-11-21 19:26:00 +0100

    Finished the automatic input of the crashdata

--- project/pipelinedata.py
+++ project/pipelinedata.py
@@ -1,25 +1,98 @@
-import pandas as pd 
+import pandas as pd
+from io import BytesIO
+import requests
+import zipfile 
 
-# Daten aus csv Quelle einlesen
-df = pd.read_table('https://raw.githubusercontent.com/Pelendones/MADE-project/main/rawdata/produkt_rr_stunde_19970707_20221231_03379.txt', sep=';')
+def load_weather_data():
+    # Erster Datenquelle für die Wetterdaten
+    df = pd.read_table('https://raw.githubusercontent.com/Pelendones/MADE-project/main/rawdata/produkt_rr_stunde_19970707_20221231_03379.txt', sep=';')
 
-#Nur auf relevante Spalten reduzieren
-df = df[['STATIONS_ID', 'MESS_DATUM', 'QN_8', '  R1']]
+    # Nur auf relevante Spalten reduzieren
+    df = df[['STATIONS_ID', 'MESS_DATUM', 'QN_8', '  R1']]
 
-#Festlegen der Datentypen
-dtypes = {'STATIONS_ID': int, 'MESS_DATUM': int, 'QN_8': int, '  R1': float}
-df = df.astype(dtypes)
+    # Festlegen der Datentypen
+    dtypes = {'STATIONS_ID': int, 'MESS_DATUM': int, 'QN_8': int, '  R1': float}
+    df = df.astype(dtypes)
 
-#Bessere Metanamen für die Tabellen
-column_mapping = {'QN_8': 'QUALITY', '  R1': 'RAINFALL'}
-df = df.rename(columns=column_mapping)
+    # Bessere Metanamen für die Tabellen
+    column_mapping = {'QN_8': 'QUALITY', '  R1': 'RAINFALL'}
+    df = df.rename(columns=column_mapping)
 
-#Muenchen Stadt als Ort eintragen
-df['Location'] = 'Muenchen-Stadt'
+    # Muenchen Stadt als Ort eintragen
+    df['Location'] = 'Muenchen-Stadt'
 
-#Schreiben in eine sqlite Datei mit dem Namen airports.sqlite und dem Tabellennamen airports
-df.to_sql('rainweather', 'sqlite:///MADE-project/data/rainweather.sqlite', if_exists='replace', index=False) 
+    # Schreiben in eine sqlite Datei rainweather.sqlite
+    df.to_sql('rainweather', 'sqlite:///MADE-project/data/rainweather.sqlite', if_exists='replace', index=False)
+    print('Done loading the weather database')
 
-#df = pd.read_table('https://raw.githubusercontent.com/Pelendones/MADE-project/main/data/produkt_rr_stunde_19970707_20221231_03379.txt', sep=';')
+def load_accident_data(year):
+    # Zweite Datenquelle für die Unfalldaten
+    zip_url = f'https://www.opengeodata.nrw.de/produkte/transport_verkehr/unfallatlas/Unfallorte{year}_EPSG25832_CSV.zip'
+    response = requests.get(zip_url)
 
-#df.to_sql('crashes','sqlite:///crashes.sqlite',if_exists='replace', index=False )
+    # Wenn Anfrage erfolgreich
+    if response.status_code == 200:
+        # Zip öffnen und extrahieren
+         with zipfile.ZipFile(BytesIO(response.content), 'r') as zip_ref:
+            # Kleine Abweichungen bei den zips ausgleichen
+            file_to_read = f'csv/Unfallorte{year}_LinRef.txt'
+
+            if year == '2016':
+                file_to_read = f'csv/Unfallorte_{year}_LinRef.txt' 
+
+            if year == '2020':
+                file_to_read = f'csv/Unfallorte{year}_LinRef.csv'
+            
+            if year == '2021':
+                file_to_read = f'Unfallorte{year}_EPSG25832_CSV.csv'
+
+            if year == '2022':
+                file_to_read = f'Unfallorte{year}_LinRef.csv' 
+
+            with zip_ref.open(file_to_read) as file:
+                df_zip = pd.read_table(file, sep=';')
+
+            # Weitere Abweichugen vereinheitlichen
+            if year == '2018':
+                column_mapping = {'OBJECTID_1': 'OBJECTID'}
+                df_zip = df_zip.rename(columns=column_mapping)     
+
+            # Nur auf relevante Spalten reduzieren
+            df_zip = df_zip[['OBJECTID', 'ULAND', 'UREGBEZ', 'UKREIS', 'UGEMEINDE', 'UJAHR', 'UMONAT', 'USTUNDE', 'UWOCHENTAG', 'UKATEGORIE', 'UART', 'UTYP1', 'IstRad']]
+
+            # Festlegen der Datentypen
+            dtypes_accidents = {
+                'OBJECTID': int,
+                'ULAND': str,
+                'UREGBEZ': int,
+                'UKREIS': int,
+                'UGEMEINDE': int,
+                'UJAHR': int,
+                'UMONAT': int, 
+                'USTUNDE': int,
+                'UWOCHENTAG': int,
+                'UKATEGORIE': int,
+                'UART': int,
+                'UTYP1': int, 
+                'IstRad': int, 
+                }
+            df_zip = df_zip.astype(dtypes_accidents)
+
+            # Schreiben in eine sqlite Datei mit dem Namen crashes.sqlite
+            df_zip.to_sql('crashes', f'sqlite:///MADE-project/data/crashes{year}.sqlite', if_exists='replace', index=False)
+            print(f'Done loading the {year} crash database') 
+
+
+def main(): 
+    load_weather_data()
+    load_accident_data(year='2016')
+    load_accident_data(year='2017')
+    load_accident_data(year='2018')
+    load_accident_data(year='2019')
+    load_accident_data(year='2020')
+    load_accident_data(year='2021')
+    load_accident_data(year='2022') 
+
+
+if __name__ == "__main__":
+    main()
