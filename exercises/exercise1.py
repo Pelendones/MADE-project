a9eeb07a265a086de14c6dfa21d85e35455d6aee
Pelendones <jean-philipp.schaller@fau.de>
2023-11-28 18:06:30 +0100

    exercise 2

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -3,5 +3,5 @@ import pandas as pd
 # Daten aus csv Quelle einlesen
 df = pd.read_csv('https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv', sep=';') 
 
-#Schreiben in eine sqlite Datei mit dem Namen airports.sqlite und dem Tabellennamen airports
+# Schreiben in eine sqlite Datei mit dem Namen airports.sqlite und dem Tabellennamen airports
 df.to_sql('airports','sqlite:///airports.sqlite',if_exists='replace', index=False )
